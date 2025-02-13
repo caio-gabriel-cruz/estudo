2f86fc8201a258f980808e2f47f87966b6e26a1a
caio-gabriel-cruz <killua37pt@gmail.com>
2025-02-12 21:56:19 -0300

    1.1.2.0

--- app/src/main/java/br/com/etecia/myapp/MenuPrincipalActivity.java
+++ app/src/main/java/br/com/etecia/myapp/MenuPrincipalActivity.java
@@ -1,15 +1,18 @@
 package br.com.etecia.myapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MenuPrincipalActivity extends AppCompatActivity {
-
+public class MenuPrincipalActivity extends AppCompatActivity implements View.OnClickListener {
+    CardView sobre;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +23,18 @@ public class MenuPrincipalActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        sobre = findViewById(R.id.sobrebtn);
+        sobre.setOnClickListener(this);
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+    if(v.getId() == R.id.sobrebtn){
+        startActivity(new Intent(getApplicationContext(), SobreNosActivity.class));
+        finish();
+    }
     }
 }
