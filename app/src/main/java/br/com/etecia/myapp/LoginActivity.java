42d188b8dc71f0b69d93d6467cb90b5c20830f6a
caio-gabriel-cruz <killua37pt@gmail.com>
2025-02-12 21:11:31 -0300

    1.1.0.0

--- app/src/main/java/br/com/etecia/myapp/LoginActivity.java
+++ app/src/main/java/br/com/etecia/myapp/LoginActivity.java
@@ -1,6 +1,10 @@
 package br.com.etecia.myapp;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,8 +12,14 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class LoginActivity extends AppCompatActivity {
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputEditText;
 
+public class LoginActivity extends AppCompatActivity {
+    MaterialButton btnEsqAS, btnCads,btnlogin;
+    TextInputEditText txtEmail,txtSenha;
+
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +30,45 @@ public class LoginActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        btnlogin = findViewById(R.id.btn_Login);
+        btnEsqAS = findViewById(R.id.btn_esqAS);
+        btnCads = findViewById(R.id.btn_cads);
+        txtEmail = findViewById(R.id.txtEmail);
+        txtSenha = findViewById(R.id.txtSenha);
+
+        btnEsqAS.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), EsqueceuSenhaActivity.class));
+                finish();
+            }
+        });
+
+        btnlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email, password;
+
+                email = txtEmail.getText().toString().trim();
+                password = txtSenha.getText().toString().trim();
+
+                if (email.equals("laercio") && password.equals("laercio")){
+                    startActivity(new Intent(getApplicationContext(), MenuPrincipalActivity.class));
+                    finish();
+                } else{
+                    Toast.makeText(getApplicationContext(), "Usuario ou senha invalidos", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(getApplicationContext(), "Usuário ou senha inválidos", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        btnCads.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),CadastroActivity.class));
+            }
+        });
+
     }
 }
